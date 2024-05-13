e7d1121e429a4ad60f5384e55575e44c35ed5bc0
ikkyu <66011476@kmitl.ac.th>
2024-05-13 23:24:17 +0700

    first commit

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from tkinter import Button, Label, Entry, CENTER, FLAT
 
 root = Tk()
 root.title("DAVE THE DIVER CALCULATOR")
+root.iconbitmap("Icon/h.ico")
 root.geometry("580x980+665+135")
 root.minsize(580, 980)
 root.maxsize(580, 980)
 root.configure(bg="#e7e7e7")
 
 custom_font = font.Font(family='Snowstorm', size=12)
-custom_font = font.Font(family='Snowstorm Light', size=12)
 
 # Set image
 # C
@@ -165,125 +165,184 @@ photoclose = ImageTk.PhotoImage(imageclose)
 
 
 # Set display value
-content = ""
+content = "0"
 txt_input = StringVar(value="0")
 
 # Function
+def set_content_and_adjust_display_size():
+    global content
+    if len(content) > 23:
+        content = "0"
+    txt_input.set(content)
+    if len(content) > 20:
+        display.config(font=("Snowstorm", 30))
+    elif len(content) > 19:
+        display.config(font=("Snowstorm", 31))
+    elif len(content) > 18:
+        display.config(font=("Snowstorm", 33))
+    elif len(content) > 17:
+        display.config(font=("Snowstorm", 36))
+    elif len(content) > 16:
+        display.config(font=("Snowstorm", 37))
+    elif len(content) > 15:
+        display.config(font=("Snowstorm", 40))
+    elif len(content) > 14:
+        display.config(font=("Snowstorm", 42))
+    elif len(content) > 13:
+        display.config(font=("Snowstorm", 45))
+    elif len(content) > 12:
+        display.config(font=("Snowstorm", 48))
+    elif len(content) > 11:
+        display.config(font=("Snowstorm", 52))
+    elif len(content) > 10:
+        display.config(font=("Snowstorm", 57))
+    elif len(content) > 9:
+        display.config(font=("Snowstorm", 63))
+    else:
+        display.config(font=("Snowstorm", 65))
+
 def btn(numbers):
     global content
+    if content == "0":
+        content = str(numbers)
+        set_content_and_adjust_display_size()
+        return 0
     content = content + str(numbers)
-    txt_input.set(content)
+    set_content_and_adjust_display_size()
 
 def equal():
     global content
+    if content[-1] in ["+", "-", "*", "/"]:
+        return 0
     try:
         result = eval(content)
         if not isinstance(result, int):
             result = float(result)
         if len(str(result)) > 10:
-            content = f"{result:.2e}"
+            content = f"{result:.2e}".upper()
         else:
             content = str(result)
+    except ZeroDivisionError:
+        content = "Infinity"
     except Exception as e:
         content = "Error"
-    txt_input.set(content)
+    set_content_and_adjust_display_size()
 
 def clear():
-    global content
-    content = ""
-    txt_input.set("")
-    display.insert(0,"0")
+    global content, txt_input
+    content = "0"
+    txt_input.set(content)
+    set_content_and_adjust_display_size()
 
 def plus():
     global content
-    if "+" not in content:
-        content = content + "+"
-        txt_input.set(content)
-    if "-" in content:
-        content = content.replace("-", "")
-        txt_input.set(content)
-    if "*" in content:
-        content = content.replace("*", "")
-        txt_input.set(content)
-    if "/" in content:
-        content = content.replace("/", "")
-        txt_input.set(content)
+    if content[-1] in ["+", "-", "*", "/"]:
+        content = content[:-1]
+    try:
+        result = eval(content)
+        if len(str(result)) > 9:
+            content = "{:.5e}".format(result).upper() + "+"
+        else:
+            content = str(result) + "+"
+    except Exception as e:
+        content = "Error"
+    set_content_and_adjust_display_size()
 
 def minus():
     global content
-    if "-" not in content:
-        content = content + "-"
-        txt_input.set(content)
-    if "+" in content:
-        content = content.replace("+", "")
-        txt_input.set(content)
-    if "*" in content:
-        content = content.replace("*", "")
-        txt_input.set(content)
-    if "/" in content:
-        content = content.replace("/", "")
-        txt_input.set(content)
+    if content[-1] in ["+", "-", "*", "/"]:
+        content = content[:-1]
+    try:
+        result = eval(content)
+        if len(str(result)) > 9:
+            content = "{:.5e}".format(result).upper() + "-"
+        else:
+            content = str(result) + "-"
+    except Exception as e:
+        content = "Error"
+    set_content_and_adjust_display_size()
 
 def multiply():
     global content
-    if "*" not in content:
-        content = content + "*"
-        txt_input.set(content)
-    if "+" in content:
-        content = content.replace("+", "")
-        txt_input.set(content)
-    if "-" in content:
-        content = content.replace("-", "")
-        txt_input.set(content)
-    if "/" in content:
-        content = content.replace("/", "")
-        txt_input.set(content)
+    if content[-1] in ["+", "-", "*", "/"]:
+        content = content[:-1]
+    try:
+        result = eval(content)
+        if len(str(result)) > 9:
+            content = "{:.5e}".format(result).upper() + "*"
+        else:
+            content = str(result) + "*"
+    except Exception as e:
+        content = "Error"
+    set_content_and_adjust_display_size()
 
 def divide():
     global content
-    if "/" not in content:
-        content = content + "/"
-        txt_input.set(content)
-    if "+" in content:
-        content = content.replace("+", "")
-        txt_input.set(content)
-    if "-" in content:
-        content = content.replace("-", "")
-        txt_input.set(content)
-    if "*" in content:
-        content = content.replace("*", "")
-        txt_input.set(content)
-
+    if content[-1] in ["+", "-", "*", "/"]:
+        content = content[:-1]
+    try:
+        result = eval(content)
+        if len(str(result)) > 9:
+            content = "{:.5e}".format(result).upper() + "/"
+        else:
+            content = str(result) + "/"
+    except Exception as e:
+        content = "Error"
+    set_content_and_adjust_display_size()
+    
 def plusminus():
     global content
-    if content[0] == "-":
-        content = content[1:]
+    if content == "0" or content == "-" or content == "0+" or content == "0-" or content == "0*" or content == "0/":
+        content = "0"
+        set_content_and_adjust_display_size()
+        return 0
+    if content[-1] in ["+", "-", "*", "/"]:
+        if content[-1] == "-" and (content[-2] in ["+", "-", "*", "/"] or len(content) == 1):
+            content = content[:-1] 
+        elif content[0] == "-":
+            content = content[1:] 
+        else:
+            content = "-" + content 
     else:
-        content = "-" + content
-    txt_input.set(content)
+        if content[0] == "-":
+            content = content[1:] 
+        elif content[0] != "-":
+            content = "-" + content  
+    set_content_and_adjust_display_size()
 
 def percent():
     global content
+    if content == "0":
+        return 0
+    if content[-1] in ["+", "-", "*", "/"]:
+        content = content + "0"
+        set_content_and_adjust_display_size()
+        return 0
+    if content[-2:] in ["+0", "-0", "*0", "/0"]:
+        return 0
     calculate = float(content) / 100
     content = str(calculate)
-    txt_input.set(calculate)
+    set_content_and_adjust_display_size()
 
 def delete():
     global content
+    if content == "0":
+        return 0
     content = content[:-1]
-    txt_input.set(content)
+    set_content_and_adjust_display_size()
 
 def dot():
     global content
     if "." not in content:
-        content = content + "."
-        txt_input.set(content)
+        content = content + ".0"
+        set_content_and_adjust_display_size()
 
 def select():
     global last_pressed
     if last_pressed is not None:
         last_pressed.invoke()
-
+        set_content_and_adjust_display_size()
+        
 def close():
     root.destroy()
 
@@ -299,43 +358,11 @@ outer_frame.place(x=32, y=82)
 inner_frame = Frame(outer_frame, bg='#b1b4a2', bd=3)
 inner_frame.place(x=0, y=0, width=485, height=102)
 
-display = Entry(inner_frame, font=("Snowstorm",64), justify="right", bd=0, fg="#474f3d", bg="#b1b4a2", textvariable=txt_input, insertwidth=0)
+display = Entry(inner_frame, font=("Snowstorm",65), justify="right", bd=0, fg="#474f3d", bg="#b1b4a2", textvariable=txt_input, insertwidth=0)
 display.place(x=0, y=0, width=470, height=100)
 
 # Button
 last_pressed = None
-
-class CustomButton(Button):
-    def __init__(self, master, photonormal, photoonenter, photocpressed, cnf={}, **kw):
-        Button.__init__(self, master, image=photonormal, **cnf, **kw)
-        self.photonormal = photonormal
-        self.photoonenter = photoonenter
-        self.photocpressed = photocpressed
-        self.config(
-            relief=FLAT,
-            borderwidth=0,
-            highlightthickness=0,
-            activebackground=self.cget("background"),
-            overrelief=FLAT
-        )
-        self.bind("<ButtonPress-1>", self.on_press)
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
-
-    def on_press(self, event):
-        global last_pressed
-        if last_pressed is not None:
-            last_pressed.config(image=last_pressed.photonormal)
-        self.config(image=self.photocpressed)
-        last_pressed = self
-
-    def on_enter(self, event):
-        if self is not last_pressed:
-            self.config(image=self.photoonenter)
-
-    def on_leave(self, event):
-        if self is not last_pressed:
-            self.config(image=self.photonormal)
 
 class PlainButton(tkinter.Button):
     def __init__(self, master=None, command=None, fg=None, bg=None, **kwargs):
@@ -348,54 +375,82 @@ class PlainButton(tkinter.Button):
             overrelief=tkinter.FLAT
         )
 
+class CustomButton(PlainButton):
+    def __init__(self, master, photonormal, photoonenter, photocpressed, position, cnf={}, **kw):
+        super().__init__(master, image=photonormal, **cnf, **kw)
+        self.photonormal = photonormal
+        self.photoonenter = photoonenter
+        self.photocpressed = photocpressed
+        self.position = position
+        self.bind("<ButtonPress-1>", self.on_press)
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_press(self, event):
+        global last_pressed
+        if last_pressed is not None:
+            last_pressed.config(image=last_pressed.photonormal)
+        self.config(image=self.photocpressed)
+        last_pressed = self
+        global current_position
+        current_position = self.position
+
+    def on_enter(self, event):
+        if self is not last_pressed:
+            self.after(0, lambda: self.config(image=self.photoonenter))
+
+    def on_leave(self, event):
+        if self is not last_pressed:
+            self.after(0, lambda: self.config(image=self.photonormal))
+
 # row1
-btnc = CustomButton(root, normalc, enterc, pressedc, command=clear, fg="#3b3838", bg="#e7e7e7")
+btnc = CustomButton(root, normalc, enterc, pressedc, (0,0),command=clear, fg="#3b3838", bg="#e7e7e7")
 btnc.place(x=50, y=268)
-btnplusminus = CustomButton(root, normalplusminus, enterplusminus, pressedplusminus, command=plusminus,fg="#3b3838", bg="#e7e7e7")
+btnplusminus = CustomButton(root, normalplusminus, enterplusminus, pressedplusminus, (0,1),command=plusminus,fg="#3b3838", bg="#e7e7e7")
 btnplusminus.place(x=172, y=268)
-btnpercent = CustomButton(root, normalpercent, enterpercent, pressedpercent, command=percent,fg="#3b3838", bg="#e7e7e7")
+btnpercent = CustomButton(root, normalpercent, enterpercent, pressedpercent, (0,2),command=percent,fg="#3b3838", bg="#e7e7e7")
 btnpercent.place(x=295, y=268)
-btndivide = CustomButton(root, normaldivide, enterdivide, presseddivide, command=divide,fg="#3b3838", bg="#e7e7e7")
+btndivide = CustomButton(root, normaldivide, enterdivide, presseddivide, (0,3),command=divide,fg="#3b3838", bg="#e7e7e7")
 btndivide.place(x=417, y=268)
 
 # row2
-btn7 = CustomButton(root, normal7, enter7, pressed7, command=lambda:btn(7),fg="#3b3838", bg="#e7e7e7")
+btn7 = CustomButton(root, normal7, enter7, pressed7, (1,0),command=lambda:btn(7),fg="#3b3838", bg="#e7e7e7")
 btn7.place(x=50, y=373)
-btn8 = CustomButton(root, normal8, enter8, pressed8, command=lambda:btn(8),fg="#3b3838", bg="#e7e7e7")
+btn8 = CustomButton(root, normal8, enter8, pressed8, (1,1),command=lambda:btn(8),fg="#3b3838", bg="#e7e7e7")
 btn8.place(x=172, y=373)
-btn9 = CustomButton(root, normal9, enter9, pressed9, command=lambda:btn(9),fg="#3b3838", bg="#e7e7e7")
+btn9 = CustomButton(root, normal9, enter9, pressed9, (1,2),command=lambda:btn(9),fg="#3b3838", bg="#e7e7e7")
 btn9.place(x=295, y=373)
-btnmultiply = CustomButton(root, normalmultiply, entermultiply, pressedmultiply, command=multiply,fg="#3b3838", bg="#e7e7e7")
+btnmultiply = CustomButton(root, normalmultiply, entermultiply, pressedmultiply, (1,3),command=multiply,fg="#3b3838", bg="#e7e7e7")
 btnmultiply.place(x=417, y=373)
 
 # row3
-btn4 = CustomButton(root, normal4, enter4, pressed4, command=lambda:btn(4),fg="#3b3838", bg="#e7e7e7")
+btn4 = CustomButton(root, normal4, enter4, pressed4, (2,0),command=lambda:btn(4),fg="#3b3838", bg="#e7e7e7")
 btn4.place(x=50, y=478)
-btn5 = CustomButton(root, normal5, enter5, pressed5, command=lambda:btn(5),fg="#3b3838", bg="#e7e7e7")
+btn5 = CustomButton(root, normal5, enter5, pressed5, (2,1),command=lambda:btn(5),fg="#3b3838", bg="#e7e7e7")
 btn5.place(x=172, y=478)
-btn6 = CustomButton(root, normal6, enter6, pressed6, command=lambda:btn(6),fg="#3b3838", bg="#e7e7e7")
+btn6 = CustomButton(root, normal6, enter6, pressed6, (2,2),command=lambda:btn(6),fg="#3b3838", bg="#e7e7e7")
 btn6.place(x=295, y=478)
-btnminus = CustomButton(root, normalminus, enterminus, pressedminus, command=minus,fg="#3b3838", bg="#e7e7e7")
+btnminus = CustomButton(root, normalminus, enterminus, pressedminus, (2,3),command=minus,fg="#3b3838", bg="#e7e7e7")
 btnminus.place(x=417, y=478)
 
 # row4
-btn1 = CustomButton(root, normal1, enter1, pressed1, command=lambda:btn(1),fg="#3b3838", bg="#e7e7e7")
+btn1 = CustomButton(root, normal1, enter1, pressed1, (3,0),command=lambda:btn(1),fg="#3b3838", bg="#e7e7e7")
 btn1.place(x=50, y=583)
-btn2 = CustomButton(root, normal2, enter2, pressed2, command=lambda:btn(2),fg="#3b3838", bg="#e7e7e7")
+btn2 = CustomButton(root, normal2, enter2, pressed2, (3,1),command=lambda:btn(2),fg="#3b3838", bg="#e7e7e7")
 btn2.place(x=172, y=583)
-btn3 = CustomButton(root, normal3, enter3, pressed3, command=lambda:btn(3),fg="#3b3838", bg="#e7e7e7")
+btn3 = CustomButton(root, normal3, enter3, pressed3, (3,2),command=lambda:btn(3),fg="#3b3838", bg="#e7e7e7")
 btn3.place(x=295, y=583)
-btnplus = CustomButton(root, normalplus, enterplus, pressedplus, command=plus,fg="#3b3838", bg="#e7e7e7")
+btnplus = CustomButton(root, normalplus, enterplus, pressedplus, (3,3),command=plus,fg="#3b3838", bg="#e7e7e7")
 btnplus.place(x=417, y=583)
 
 # row5
-btndelete = CustomButton(root, normaldelete, enterdelete, presseddelete, command=delete,fg="#3b3838", bg="#e7e7e7")
+btndelete = CustomButton(root, normaldelete, enterdelete, presseddelete, (4,0),command=delete,fg="#3b3838", bg="#e7e7e7")
 btndelete.place(x=50, y=688)
-btn0 = CustomButton(root, normal0, enter0, pressed0, command=lambda:btn(0),fg="#3b3838", bg="#e7e7e7")
+btn0 = CustomButton(root, normal0, enter0, pressed0, (4,1),command=lambda:btn(0),fg="#3b3838", bg="#e7e7e7")
 btn0.place(x=172, y=688)
-btndot = CustomButton(root, normaldot, enterdot, presseddot, command=dot,fg="#3b3838", bg="#e7e7e7")
+btndot = CustomButton(root, normaldot, enterdot, presseddot, (4,2),command=dot,fg="#3b3838", bg="#e7e7e7")
 btndot.place(x=295, y=688)
-btnequal = CustomButton(root, normalequal, enterequal, pressedequal, command=equal,fg="#3b3838", bg="#e7e7e7")
+btnequal = CustomButton(root, normalequal, enterequal, pressedequal, (4,3),command=equal,fg="#3b3838", bg="#e7e7e7")
 btnequal.place(x=417, y=688)
 
 # row6
@@ -403,5 +458,7 @@ btnselect = PlainButton(root, image=photoselect, command=select, fg="#3b3838", b
 btnselect.place(x=108, y=845)
 btnspace = PlainButton(root, image=photoclose, command=close, fg="#3b3838", bg="#e7e7e7")
 btnspace.place(x=301, y=845)
+
+
 
 root.mainloop()
